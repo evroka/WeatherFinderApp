e7cc2b3c13daba39ee0bb3aa7d1df3c85bd1cbc0
evroka <nata_safonova@inbox.ru>
2018-12-16 19:38:18 +0400

    Add props to components and styles

--- src/App.js
+++ src/App.js
@@ -23,14 +23,25 @@ class App extends React.Component {
 		const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
 		const data = await apiCall.json()
 
-		this.setState({
-			temperature: data.main.temp,
-			city: data.name,
-			country: data.sys.country,
-			humidity: data.main.humidity,
-			description: data.weather[0].description,
-			error: ""
-		})
+		if (city && country) {
+			this.setState({
+				temperature: data.main.temp,
+				city: data.name,
+				country: data.sys.country,
+				humidity: data.main.humidity,
+				description: data.weather[0].description,
+				error: ""
+			})
+		} else {
+			this.setState({
+				temperature: undefined,
+				city: undefined,
+				country: undefined,
+				humidity: undefined,
+				description: undefined,
+				error:'Please enter value'
+			})
+		}
 	}
 
 	render() {
@@ -44,16 +55,28 @@ class App extends React.Component {
 		 } = this.state
 		return (
 			<div>
-				<Titles />
-				<Form getWeather={this.getWeather} />
-				<Weather 
-					temperature={temperature} 
-					city={city}
-					country={country}
-					humidity={humidity}
-					description={description}
-					error={error}
-				/>
+				<div className='wrapper'>
+					<div className='main'>
+						<div className='container'>
+							<div className='row'>
+								<div className='col-xs-5 title-container'>
+									<Titles />
+								</div>
+								<div className='col-xs-7 form-container'>
+									<Form getWeather={this.getWeather} />
+									<Weather 
+										temperature={temperature} 
+										city={city}
+										country={country}
+										humidity={humidity}
+										description={description}
+										error={error}
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,20 +1,41 @@
 import React from 'react'
 
-
-class Weather extends React.Component {
-	render() {
-		const { temperature, city, country, humidity, description } = this.props
-		return (
-			<div>
-				{city && country &&
-					<p>Location: {city} , { country }</p>
-				}
-				{temperature && <p>Temperature: { temperature }</p>}
-				{humidity && <p>Humidity: { humidity }</p>}
-				{description && <p>Conditions: { description }</p>}
-			</div>
-		)
-	}
+const Weather = (props) => {
+	const { 
+		temperature, 
+		city,
+		country,
+		humidity,
+		description,
+		error
+	 } = props
+	 return (
+		<div className='weather-info'>
+			{city && country && 
+				<p className='weather-param'>Location: 
+					<span className='weather-value'> {city} , { country }</span>
+				</p>
+			}
+			{temperature && 
+				<p className='weather-param'>Temperature: 
+					<span className='weather-value'> { temperature }</span>
+				</p>
+			}
+			{humidity && 
+				<p className='weather-param'>Humidity: 
+					<span className='weather-value'> { humidity }</span>
+				</p>
+			}	
+			{description && 
+				<p className='weather-param'>Conditions: 
+					<span className='weather-value'> { description }</span>
+				</p>
+			}
+			{error && 
+				<p className='weather-error'> { error }</p> 
+			}
+		</div>
+	)
 }
 
 export default Weather
